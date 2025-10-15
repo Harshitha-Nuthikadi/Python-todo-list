6ec25a46e5643a8ac9f35e1a88a9db3fb6ca4cd8
Harshitha Nuthikadi <harshithanuthikadi@gmail.com>
2025-10-15 10:18:27 +0530

    Delete continue.py

--- continue.py
+++
@@ -1,5 +0,0 @@
-# the continue statement makes Python skip the rest of the loop body and go to the next number.
-for num in range(1, 11):
-    if num % 3 == 0:
-        continue  # Skip this number and move to the next iteration
-    print(num)
